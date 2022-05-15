004c5ed736854986c1f1428d1c430e7930e153f3
Dawn Olivier <dmarieolivier@gmail.com>
2022-05-14 20:05:16 -0400

    finished mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildTask(e.target['new-task-description'].value)
+    form.reset()
+  })
+})
+
+function buildTask(task){
+let li = document.createElement('li')
+let btn = document.createElement('button')
+btn.addEventListener('click', handleDelete)
+btn.textContent = 'x'
+li.textContent = `${task} `
+li.appendChild(btn)
+document.querySelector('#tasks').appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
